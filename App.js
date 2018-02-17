4f2c5a9b64b359f2629355a8817e0d2c714a169b
jamesalbert <jimmychanga.cheese@gmail.com>
2018-02-17 13:03:56 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import { StyleSheet, Button, Text, TextInput, View, AsyncStorage } from 'react-native';
+var CryptoJS = require("crypto-js");
+
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: null,
+      secret: null,
+      balance: 0.0,
+      availBalance: 0.0,
+      holdBalance: 0.0,
+      price: 0.0,
+      endpoint: 'https://coinfalcon.com/api/v1'
+    }
+    this.refresh = this.refresh.bind(this)
+    this.headers = this.headers.bind(this)
+    this.getSecrets = this.getSecrets.bind(this)
+    this.getSecrets();
+  }
+
+  async getSecrets() {
+    try {
+      const key = await AsyncStorage.getItem('@ClovePurse:key');
+      const secret = await AsyncStorage.getItem('@ClovePurse:secret');
+      this.setState(prev => {
+        return {
+          key: key,
+          secret: secret
+        }
+      })
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  async setSecrets() {
+    try {
+      await AsyncStorage.setItem('@ClovePurse:key', this.state.key);
+      await AsyncStorage.setItem('@ClovePurse:secret', this.state.secret);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  async clearSecrets() {
+    try {
+      await AsyncStorage.removeItem('@ClovePurse:key');
+      await AsyncStorage.removeItem('@ClovePurse:secret');
+      this.setState(prev => {
+        return {
+          key: null,
+          secret: null
+        }
+      });
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  headers(route) {
+    timestamp = Math.floor(new Date() / 1000);
+    payload = [timestamp, 'GET', route].join('|');
+    signature = CryptoJS.HmacSHA256(payload, this.state.secret);
+    return {
+      "CF-API-KEY": this.state.key,
+      "CF-API-TIMESTAMP": timestamp.toString(),
+      "CF-API-SIGNATURE": signature.toString()
+    }
+  }
+
+  refresh() {
+    this.setSecrets();
+    var headers = this.headers('/api/v1/user/accounts');
+    fetch(this.state.endpoint + '/user/accounts', {
+      method: 'GET',
+      headers: headers
+    }).then(resp => {
+      data = JSON.parse(resp._bodyInit).data;
+      grlc = data[6]
+      this.setState(prev => {
+        return {
+          balance: grlc.balance,
+          availBalance: grlc.available_balance,
+          holdBalance: grlc.hold_balance
+        }
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>ClovePurse</Text>
+        <Text>GRLC/USD: ${this.state.price || 0.0}</Text>
+        <Text>Balance: {this.state.balance || 0.0} GRLC</Text>
+        <Text>Availble Balance: {this.state.availBalance || 0.0} GRLC</Text>
+        <Text>On Hold Balance: {this.state.holdBalance || 0.0} GRLC</Text>
+
+        <TextInput
+          style={{height: 40, width: "80%", textAlign: "center" }}
+          placeholder={this.state.key || "api key"}
+          onChangeText={(key) => this.setState({key})}
+        />
+        <TextInput
+          style={{height: 40, width: "80%", textAlign: "center" }}
+          placeholder={this.state.secret || "api secret"}
+          onChangeText={(secret) => this.setState({secret})}
+        />
+        <Button
+          onPress={this.refresh}
+          title="refresh"
+          color="grey"
+          accessibilityLabel="Refresh Garlicoin Wallet Balance"
+        />
+        <Button
+          onPress={this.clearSecrets}
+          title="clear"
+          color="red"
+          accessibilityLabel="Refresh Garlicoin Wallet Balance"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+ClovePurse
+==========
+
+iOS app that shows your current balance of Garlicoin using CoinFalcon's API.
+
+## Getting Started
+
+Clone and cd into this repo and run `yarn run ios` to start the packager and
+and iOS simulator. Enter your api key and secret from CoinFalcon and click "refresh".
+
+
+This is an intro app for /u/jbdawinna, finish it up bruh. See if you can get the
+balance with just the wallet address and take out the api key and secret stuff.
